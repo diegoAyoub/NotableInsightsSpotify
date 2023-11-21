f0bd531effcac5d9eef58cda56d279db093ab40e
dayoub <88570152+FarshidDiego@users.noreply.github.com>
2023-11-20 20:52:35 -0800

    tooltip, responsive to sort. Need to fix scale

--- js/RectangleChart.js
+++ js/RectangleChart.js
@@ -16,8 +16,10 @@ class RectChart {
           bottom: 20,
           left: 30
         },
-        
-        // Todo: Add or remove attributes from config as needed
+        tooltipPadding: 5,
+        selectedAttribute: 'danceability',
+        selectedYear: '2021',
+        sortBy: 'popularity'
       }
       this.data = data;
       this.initVis();
@@ -41,7 +43,9 @@ class RectChart {
           .domain([0,1])
           .range(['#ffffff', '#7733ff']);
       
-      vis.xAxis = d3.axisBottom(vis.xScale);
+      vis.xAxis = d3.axisBottom(vis.xScale)
+          .tickFormat(d => '')
+          .tickSize(0);
   
       vis.yAxis = d3.axisLeft(vis.yScale)
           .tickSize(1)
@@ -68,19 +72,6 @@ class RectChart {
           .attr('class', 'axis y-axis')
           .attr('transform', `translate(0,0)`);
       
-      
-      
-  
-      // axis titles
-      // vis.chart.append('text')
-      //     .attr('class', 'axis-title')
-      //     .attr('y', vis.height)
-      //     .attr('x', 0)
-      //     .attr('dy', 30)
-      //     .attr('dx',0)
-      //     .style('text-anchor', 'start')
-      //     .text('Accuracy');
-  
       vis.chart.append('text')
           .attr('class', 'axis-title')
           .attr('x', 0)
@@ -97,7 +88,7 @@ class RectChart {
           .attr('dy',-22)
           .attr('dx', 25)
           .attr('text-anchor', 'start')
-          .text('Year Selected: 2021');
+          .text(`Year Selected: ${vis.config.selectedYear}`);
       
       vis.chart.append('text')
         .attr('class', 'axis-title')
@@ -106,7 +97,7 @@ class RectChart {
         .attr('dy',-7)
         .attr('dx', 0)
         .attr('text-anchor', 'start')
-        .text('Attribute Selected: Danceability');
+        .text(`Attribute Selected: ${vis.config.selectedAttribute}`);
       
     vis.updateVis();
     
@@ -114,20 +105,26 @@ class RectChart {
   
     updateVis() {
       let vis = this;
-  
-      // vis.xValue = d => {
-      //   return d.pcgdp;
-      // }
       vis.xValue = d => 1;
-      vis.yValue = d => d.trackPopularity;
+      vis.yValue = d => {
+        if(vis.config.sortBy == 'popularity'){
+          return d.trackPopularity;
+        } else{
+          return d.danceability;
+        }
+      }
       vis.filteredData = vis.data.filter(d => d.year == 2021);
       console.log(vis.filteredData.length);
       vis.colorVal = d => d.danceability;
       // vis.uniqueYears = vis.filteredData.map(vis.yValue).filter(vis.uniqueFilter);
       vis.uniqueYears = vis.filteredData.map(vis.yValue);
       console.log(vis.uniqueYears.sort((a, b) => a - b));
-      vis.yScale.domain(vis.uniqueYears.sort((a,b) => a - b));
-      
+
+      if(vis.config.sortBy == 'popularity'){
+        vis.yScale.domain(vis.uniqueYears.sort((a,b) => a - b));
+      } else{
+        vis.yScale.domain(vis.filteredData.map(vis.colorVal).sort((a,b) => a - b));
+      }
       vis.renderVis();
       
     }
@@ -137,7 +134,7 @@ renderVis() {
   
   //render circles onto chart
   const rectangles = vis.chart.selectAll('.bar')
-      .data(vis.filteredData, d => d.trackPopularity)
+      .data(vis.filteredData, d => d.danceability)
       .join('rect')
       .attr('class', 'bar')
       .attr('x', 0)
@@ -147,6 +144,25 @@ renderVis() {
       .attr('fill', d => vis.colorScale(vis.colorVal(d)))
       .attr('stroke-width', 1)
       .attr('stroke', 'black');
+
+  // Tooltip mouseover event
+
+  rectangles
+      .on('mouseover', (event,d) => {
+        d3.select('#tooltip')
+          .style('display', 'block')
+          .style('left', (event.pageX + vis.config.tooltipPadding) + 'px')   
+          .style('top', (event.pageY + vis.config.tooltipPadding) + 'px')
+          .html(`
+            <div class="tooltip-bold">Artist: ${d.artist_name} </div>
+            <div class="tooltip-bold">Song: ${d.track_name} </div>
+            <div class="tooltip-text">Popularity (0-99): ${d.trackPopularity}</div>
+            <div class="tooltip-text">Atrribute (0-1): ${d.danceability} </div>
+          `);
+      })
+      .on('mouseleave', () => {
+        d3.select('#tooltip').style('display', 'none');
+      });
 
 
   //update axis
@@ -159,13 +175,8 @@ renderVis() {
       .call(g => g.select('.domain').remove());
   }
 
-
-uniqueFilter(value, index, self) {
-    return self.indexOf(value) === index;
 }
 
-sortHelper (a, b) {
-  return b - a;
-}
-
+function uniqueFilter(value, index, self) {
+  return self.indexOf(value) === index;
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-
+let rectChart;
 d3.csv('data/playlist_2010to2022.csv').then(data => {
     data.forEach(d => {
 
@@ -9,9 +9,17 @@ d3.csv('data/playlist_2010to2022.csv').then(data => {
       // Optional: other data preprocessing steps
     });
     
-    const rectChart = new RectChart({
+    rectChart = new RectChart({
       parentElement: '#rectangle-chart' 
     }, data);
 
+  });
+
+  d3.select('#sort-selector').on('change', function() {
+
+    // Get selected display type and update chart
+    rectChart.config.sortBy = d3.select(this).property('value');
+    rectChart.updateVis();
+  
   });
   
